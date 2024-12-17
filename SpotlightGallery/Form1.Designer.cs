ec193a51659cf0e784c212fb1606b0668886053c
d0gkiller87 <32069685+d0gkiller87@users.noreply.github.com>
2024-12-18 00:16:29 +0800

    First release

---
+++ SpotlightGallery/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SpotlightGallery {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose( bool disposing ) {
+      if ( disposing && ( components != null ) ) {
+        components.Dispose();
+      }
+      base.Dispose( disposing );
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.imagePanel = new System.Windows.Forms.FlowLayoutPanel();
+      this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+      this.SuspendLayout();
+      //
+      // imagePanel
+      //
+      this.imagePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.imagePanel.AutoScroll = true;
+      this.imagePanel.BackColor = System.Drawing.Color.White;
+      this.imagePanel.Location = new System.Drawing.Point(12, 12);
+      this.imagePanel.Name = "imagePanel";
+      this.imagePanel.Size = new System.Drawing.Size(776, 426);
+      this.imagePanel.TabIndex = 1;
+      //
+      // saveFileDialog1
+      //
+      this.saveFileDialog1.CheckPathExists = false;
+      this.saveFileDialog1.DefaultExt = "jpg";
+      this.saveFileDialog1.Filter = "JPEG Files|*.jpg;*.jpeg|All Files|*";
+      this.saveFileDialog1.RestoreDirectory = true;
+      this.saveFileDialog1.Title = "Save image to...";
+      //
+      // Form1
+      //
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.imagePanel);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "Form1";
+      this.Text = "SpotlightGallery v0.1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.FlowLayoutPanel imagePanel;
+    private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+  }
+}
+
---
+++ SpotlightGallery/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SpotlightGallery {
+  public partial class Form1 : Form {
+    string desktopPicPath = Environment.ExpandEnvironmentVariables(
+      @"%USERPROFILE%\AppData\Local\Packages\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\LocalCache\Microsoft\IrisService\"
+    );
+    string lockscreenPicPath = Environment.ExpandEnvironmentVariables(
+      @"%USERPROFILE%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\"
+    );
+    ContextMenuStrip contextMenu = new ContextMenuStrip();
+
+    public Form1() {
+      InitializeComponent();
+
+      contextMenu.Items.Add(
+        "Save as...", null,
+        onClick: ( object sender, EventArgs e ) => {
+          DialogResult result = saveFileDialog1.ShowDialog();
+          if ( result == DialogResult.OK ) {
+            File.Copy( ( string ) contextMenu.Tag, saveFileDialog1.FileName, true );
+          }
+        }
+      );
+
+      contextMenu.Items.Add(
+        "Open file location", null,
+        onClick: ( object sender, EventArgs e ) => {
+          OpenFileExplorerToImage( ( string ) contextMenu.Tag );
+        }
+      );
+    }
+
+    const int SW_SHOWNORMAL = 1;
+    [DllImport( "shell32.dll", CharSet = CharSet.Auto )]
+    public static extern IntPtr ShellExecute(
+        IntPtr hwnd,         // Handle to the parent window (null for no parent)
+        string lpOperation,  // Operation to perform (e.g., "open", "print", etc.)
+        string lpFile,       // The file to open
+        string lpParameters, // Parameters (optional, can be null)
+        string lpDirectory,  // Initial directory (optional, can be null)
+        int nShowCmd         // How to display the window (e.g., SW_SHOWNORMAL)
+    );
+
+    void OpenFile( string path ) {
+      ShellExecute( IntPtr.Zero, "open", path, null, null, SW_SHOWNORMAL );
+    }
+
+    void OpenFileExplorerToImage( string fullPath ) {
+      Process.Start( "explorer.exe", $"/select, \"{ fullPath }\"" );
+    }
+
+    void DrawImages( string[] picturePaths, int imageWidth = 300, string tag = "" ) {
+      foreach ( string picturePath in picturePaths ) {
+        Image image = Image.FromFile( picturePath );
+        Bitmap picture = new Bitmap( image );
+
+        double scale = ( double ) picture.Width / picture.Height;
+        if ( scale != 16.0 / 9.0 ) continue;
+
+        int newHeight = ( int ) Math.Floor( imageWidth / scale );
+        PictureBox pictureBox = new PictureBox() {
+          Image = picture,
+          SizeMode = PictureBoxSizeMode.StretchImage,
+          ClientSize = new Size( imageWidth, newHeight )
+        };
+
+        pictureBox.MouseClick += ( object sender, MouseEventArgs e ) => {
+          if ( e.Button == MouseButtons.Right ) {
+            saveFileDialog1.FileName = Path.GetFileName( picturePath );
+            contextMenu.Tag = picturePath;
+            contextMenu.Show( pictureBox, e.Location );
+          }
+        };
+
+        pictureBox.MouseDoubleClick += ( object sender, MouseEventArgs e ) => {
+          OpenFile( picturePath );
+        };
+
+        TableLayoutPanel panel = new TableLayoutPanel() {
+          AutoSize = true,
+          AutoSizeMode = AutoSizeMode.GrowAndShrink,
+          Margin = new Padding {
+            Left = 0, Right = 0,
+            Top = 1, Bottom = 1
+          },
+          RowCount = 2, ColumnCount = 1,
+        };
+
+        string pictureSize = $"{ image.Width } x { image.Height }";
+        string pictureDate = File.GetCreationTime( picturePath ).ToString( "yyyy/MM/dd" );
+
+        Label label = new Label() {
+          Text = $"{ tag }; { pictureSize } - { pictureDate }",
+          TextAlign = ContentAlignment.MiddleCenter,
+          Anchor = AnchorStyles.None,
+          AutoSize = true
+        };
+        label.Size = new Size( label.Width, 10 );
+        label.Font = new Font( "Consolas", label.Font.Size, FontStyle.Regular );
+
+        panel.Controls.Add( pictureBox );
+        panel.Controls.Add( label );
+
+        imagePanel.Controls.Add( panel );
+      }
+    }
+
+    private void Form1_Load( object sender, EventArgs e ) {
+      if ( Directory.Exists( lockscreenPicPath ) ) {
+        string[] lockscreenPics = Directory.GetFiles( lockscreenPicPath, "*.*", SearchOption.AllDirectories );
+        DrawImages( lockscreenPics, tag: "Lock Screen" );
+      }
+      if ( Directory.Exists( desktopPicPath ) ) {
+        string[] desktopPics = Directory.GetFiles( desktopPicPath , "*.*", SearchOption.AllDirectories );
+        DrawImages( desktopPics, tag: "Desktop" );
+      }
+
+      this.Size = new Size(
+        width: 320 * 4,
+        height: 180 * 4
+      );
+    }
+  }
+}
